532fb5474813df037786507f6d163c6490768ece
msleonar <msleonar@ncsu.edu>
2018-11-06 13:25:12 -0500

    Fixed config referencing incorrect sub-config names Fixed expecting php to process config file in the middle of blade

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -19,17 +19,17 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
         Blade::directive('githash', function ($length) {
             if (!is_int($length)) {
-                $length = config('hash_length');
+                $length = config('git.hash_length');
             }
 
-            return "<?php echo substr({config('git.hash')}, 0, $length); ?>";
+            return "<?php echo substr(config('git.hash'), 0, $length); ?>";
         });
         Blade::directive('gitdate', function ($format) {
             if (!is_string($format)) {
-                $format = config('commit_date_format');
+                $format = config('git.commit_date_format');
             }
 
-            return "<?php echo date($format, {config('git.date')}); ?>";
+            return "<?php echo date($format, config('git.date')); ?>";
         });
 
         $this->commands([
